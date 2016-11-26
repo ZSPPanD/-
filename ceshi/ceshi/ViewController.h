8d23291ef1a2b5b8e63fd73b49d0f5e18ef54634
ZSP <2095976617@qq.com>
2016-11-26 17:57:41 +0800

    fe

---
+++ ceshi/ceshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshi
+//
+//  Created by VID on 16/11/26.
+//  Copyright © 2016年 VID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ceshi/ceshi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ceshi
+//
+//  Created by VID on 16/11/26.
+//  Copyright © 2016年 VID. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
